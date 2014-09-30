0f78dda75811f2023b870e78ed9daff73a9616fb
Chandra Addala <chandra.addala@rackspace.com>
2014-09-30 15:48:03 -0500

    [B-78574] Move tenant entries check to servlet filter in cloudfeeds-atomhopper    - Modified Transform Utils to rely completely on firstbyte rather than status code as the filter call is async (Not completely related to story)    - Added TenantedEntryVerificationFilter to verify tenant id from response against tenantId in request

--- filters/xslt-filter/src/main/java/com/rackspace/feeds/filter/TransformerUtils.java
+++ filters/xslt-filter/src/main/java/com/rackspace/feeds/filter/TransformerUtils.java
@@ -8,14 +8,12 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -46,51 +44,61 @@ public class TransformerUtils {
             TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
 
     public void doTransform(HttpServletRequest wrappedRequest,
-                               HttpServletResponse wrappedResponse,
-                               HttpServletResponse originalResponse,
-                               FilterChain chain,
-                               StreamSource xsltStream,
-                               Map<String, Object> xsltParameters)
+                            HttpServletResponse wrappedResponse,
+                            HttpServletResponse originalResponse,
+                            FilterChain chain,
+                            StreamSource xsltStream,
+                            Map<String, Object> xsltParameters)
             throws IOException, ServletException {
-
 
         ServletResponsePipe srp = new ServletResponsePipe(wrappedRequest, wrappedResponse);
         srp.doFilterAsynch(chain);
 
-        int status = wrappedResponse.getStatus();
+        try {
+            InputStream bis = new BufferedInputStream(srp.getInputStream());
+            int firstByte = getFirstByte(bis);
+            int status = wrappedResponse.getStatus();
 
-        // If it's not 20x, then we may not get an XML or even
-        // a response body. So we only check for success code.
-        if ( status >= 200 && status <300 ) {
-
-            try {
-                // This is because we can't deterministically get Response's Content-Type
-                // from HttpServletResponse until we actually read it. Instead of
-                // relying on the Content-Type, we can look ahead 1 byte into the
-                // input stream. If it is '<', then we take our chances and pass it
-                // down to XSLT.
-                BufferedInputStream bis = new BufferedInputStream(srp.getInputStream());
-                bis.mark(1);
-                int char1 = bis.read();
-                bis.reset();
-                if ( char1 == '<') {
-                    doTransform(xsltStream,
-                                xsltParameters,
-                                new StreamSource(bis),
-                                new StreamResult(originalResponse.getWriter()));
-                } else {
-                    // the input is not XML
-                    LOG.debug("Skipping transform cuz input stream starts with '" + char1 + "', does not look to be XML");
-                    IOUtils.copy(bis, originalResponse.getWriter());
-                }
-            } catch(TransformerException te) {
-                throw new ServletException(te);
+            // This is because we can't deterministically get Response's Content-Type
+            // from HttpServletResponse until we actually read it. Instead of
+            // relying on the Content-Type, we can look ahead 1 byte into the
+            // input stream. If it is '<', then we take our chances and pass it
+            // down to XSLT.
+            if ( firstByte == '<' &&  (status >= 200 && status <300)) {
+                doTransform(xsltStream,
+                        xsltParameters,
+                        new StreamSource(bis),
+                        new StreamResult(originalResponse.getWriter()));
+            } else {
+                // the input is not XML
+                LOG.debug("Skipping transform cuz input stream starts with '" + firstByte + "', does not look to be XML or Response has status=" + wrappedResponse.getStatus());
+                IOUtils.copy(srp.getInputStream(), originalResponse.getWriter());
             }
-        } else {
-            // copy input to output as is
-            LOG.debug("Skipping transform cuz response has status=" + status);
-            IOUtils.copy(srp.getInputStream(), originalResponse.getOutputStream());
+        } catch(TransformerException te) {
+            throw new ServletException(te);
         }
+    }
+
+    /**
+     * Reads input stream and returns the first byte as integer. After reading it
+     * repositions the stream to the beginning.
+     *
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    private int getFirstByte(InputStream inputStream) throws IOException {
+
+        int firstByte = -1;
+
+        try {
+            inputStream.mark(1);
+            firstByte = inputStream.read();
+        } finally {
+            inputStream.reset();
+        }
+
+        return firstByte;
     }
 
     /**
